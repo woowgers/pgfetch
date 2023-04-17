375291979444d601696ecf9b87d36c8eca987f8b
Alexander Shibaev <sashashibaev@gmail.com>
2023-04-18 01:20:44 +0300

    add asynchronous file handling

--- src/main.py
+++ src/main.py
@@ -1,14 +1,12 @@
 import asyncio
-from tempfile import TemporaryDirectory
 import os
+from tempfile import TemporaryDirectory
 
 from fetcher import RepositoryFileFetcher
 
 
 async def main():
-    fetcher = RepositoryFileFetcher(
-        host="gitea.radium.group", org="radium", repo="project-configuration", branch="master"
-    )
+    fetcher = RepositoryFileFetcher(host="gitea.radium.group", org="radium", repo="flatfilecms", branch="master")
     tmpdir = TemporaryDirectory()
     await fetcher.save_contents(root_dir=tmpdir.name, cksum_file="sha256sums", n_tasks_max=10)
     print(*tuple(os.walk(tmpdir.name)))
