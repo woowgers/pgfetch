9d6ee7823916238cec864a1f4e4db004097f7d20
woowgers <sashashibaev@gmail.com>
2023-04-18 19:56:49 +0300

    change target repo to `project-configuration`

--- src/main.py
+++ src/main.py
@@ -12,14 +12,14 @@ def configure_logging():
     root_logger.setLevel(logging.INFO)
     handler = logging.StreamHandler(sys.stderr)
     handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     root_logger.addHandler(handler)
 
 
 async def main():
     fetcher = RepositoryFileFetcher(
-        host="gitea.radium.group", org="radium", repo="flatfilecms", branch="master"
+        host="gitea.radium.group", org="radium", repo="project-configuration", branch="master"
     )
     tmpdir = TemporaryDirectory()
     await fetcher.save_contents(root_dir=tmpdir.name, cksum_file="sha256sums")
