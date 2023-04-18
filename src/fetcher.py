3ea554942563eabbc5afbaeb1a56a4146eb23189
woowgers <sashashibaev@gmail.com>
2023-04-18 17:41:07 +0300

    WIP

--- src/fetcher.py
+++ src/fetcher.py
@@ -1,24 +1,37 @@
 import asyncio
-import aiofiles
+import logging
 from hashlib import sha256
 from pathlib import Path
-import logging
+from typing import Sequence
 
-from giteaapi import GiteaRepositoryApi
+import aiofiles
+
+from giteaapi import GiteaRepositoryApi, GitTree, FileContent
 
 
 class RepositoryFileFetcher:
     def __init__(self, host: str, org: str, repo: str, branch: str):
-        self.api = GiteaRepositoryApi(host=host, org=org, repo=repo)
-        branch_id = self.api.get_branch_id(branch)
-        self.filepaths: tuple[str, ...] = self.api.get_branch_filepaths(branch_id)
+        self.api = GiteaRepositoryApi(host=host, org=org, repo=repo, branch=branch)
 
     async def _write_filepath_cksum(
         self, filepath: str, content: bytes, cksum_file: str, cksum_file_lock: asyncio.Lock
     ):
-        async with cksum_file_lock, aiofiles.open(filepath, "a") as f:
+        async with cksum_file_lock, aiofiles.open(cksum_file, "a") as f:
             cksum = sha256(content).hexdigest()
             await f.write(f"{filepath}\t{cksum}\n")
+
+    async def save_filepath(
+        self,
+        local_filepath: Path,
+        remote_filepath: str,
+        cksum_file: str | None = None,
+        cksum_file_lock: asyncio.Lock | None = None,
+    ):
+        async with aiofiles.open(local_filepath, "w") as f:
+            content = await self.api.get_file_content(remote_filepath)
+            await f.write(content.decode())
+        if cksum_file and cksum_file_lock:
+            await self._write_filepath_cksum(remote_filepath, content, cksum_file, cksum_file_lock)
 
     async def _save_filepath_content(
         self,
@@ -28,24 +41,19 @@ class RepositoryFileFetcher:
         cksum_file: str | None = None,
         cksum_file_lock: asyncio.Lock | None = None,
     ):
-        logging.log(level=logging.INFO, msg=f"Fetching {filepath}...")
-
         if cksum_file and not cksum_file_lock:
             raise ValueError("`cksum_file` and `cksum_file_lock` must only be specified conjointly")
 
         await n_tasks_sem.acquire()
+
+        logging.log(level=logging.INFO, msg=f"Fetching {filepath}...")
 
         local_filepath = root_dir / filepath
         parent_dir = root_dir / Path(filepath).parent
         if not parent_dir.exists():
             parent_dir.mkdir(parents=True)
 
-        async with aiofiles.open(local_filepath, "w") as f:
-            content = await self.api.get_file_content(filepath)
-            await f.write(content.decode())
-
-        if cksum_file and cksum_file_lock:
-            await self._write_filepath_cksum(filepath, content, cksum_file, cksum_file_lock)
+        await self.save_filepath(local_filepath, filepath)
 
         n_tasks_sem.release()
 
@@ -58,13 +66,13 @@ class RepositoryFileFetcher:
             asyncio.create_task(
                 self._save_filepath_content(
                     root_dir=root_dir,
-                    filepath=filepath,
+                    filepath=entry.path,
                     cksum_file=str(cksum_file),
                     cksum_file_lock=cksum_file_lock,
                     n_tasks_sem=n_tasks_sem,
                 )
             )
-            for filepath in self.filepaths
+            for entry in (await self.api.get_branch_tree()).files
         )
         await asyncio.gather(*tasks)
 
@@ -91,3 +99,33 @@ class RepositoryFileFetcher:
             await self._save_contents_with_cksums(root_dir, Path(cksum_file), n_tasks_sem)
         else:
             await self._save_contents(root_dir, n_tasks_sem)
+
+    async def _write_tree_contents_with_cksums(
+        self, root_dir: Path, tree: GitTree, cksum_file: Path, cksum_file_lock: asyncio.Lock
+    ):
+        ...
+
+    async def _save_tree_contents_with_cksums(
+        self, root_dir: Path, n_tasks_sem: asyncio.Semaphore, cksum_file: Path, cksum_file_lock: asyncio.Lock, page: int = 1
+    ):
+        await n_tasks_sem.acquire()
+
+        tree = await self.api.get_branch_tree()
+
+        # tasks = (asyncio.create_task())
+
+        if not tree.complete:
+            n_tasks_sem.release()
+            return await self._save_tree_contents_with_cksums(root_dir, n_tasks_sem, cksum_file, cksum_file_lock, page + 1)
+
+        n_tasks_sem.release()
+
+
+    async def save_tree_contents_with_cksums(self, root_dir: str | Path, n_tasks_max: int, cksum_file: str | Path):
+        cksum_file = Path(cksum_file)
+        if cksum_file.exists():
+            cksum_file.unlink()
+
+        n_tasks_sem = asyncio.Semaphore(n_tasks_max)
+        cksum_file_lock = asyncio.Lock()
+        ...
